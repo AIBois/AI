e6a2658974c71d3036093e824d79d8304e507735
tieran02 <tieran.wightman@gmail.com>
2021-02-16 17:28:39 +0000

    Basic steering agent with seek behaviour

---
+++ Assets/Scripts/Steering/SeekSteeringBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SeekSteeringBehavior : SteeringBehavior
+{
+
+    public override SteeringState GetSteering(SteeringAgent agent)
+    {
+        SteeringState state = new SteeringState();
+        if (!TargetPosition.HasValue || !agent)
+            return state;
+
+        //Get direction
+        state.linear = TargetPosition.Value - agent.Position;
+        state.linear.Normalize();
+        state.linear *= agent.MaxAcceleration;
+
+        state.angular = 0.0f;
+
+        return state;
+    }
+}
---
+++ Assets/Scripts/Steering/SteeringBehaviorFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum SteeringBehaviorType
+{
+    SEEK
+}
+
+public class SteeringBehaviorFactory
+{
+    public static SteeringBehavior Create(SteeringBehaviorType steeringType)
+    {
+        switch (steeringType)
+        {
+            case SteeringBehaviorType.SEEK:
+                return new SeekSteeringBehavior();
+            default:
+                throw new ArgumentOutOfRangeException(nameof(steeringType), steeringType, null);
+        }
+    }
+}
