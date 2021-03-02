af333a6ce0f294ffb6dba2f2bc74ab2a8563cb7b
plurple <mattgrierson@btinternet.com>
2021-03-02 14:52:31 +0000

    adding the start of the squad base

---
+++ Assets/Scripts/SquadBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquadBase : MonoBehaviour
+{
+    public CharacterBase Leader;
+    public CharacterBase[] Units;
+    [SerializeField]
+    private float cost;
+
+    private void Awake()
+    {
+        //TODO:: create a randomisation of the cost based ont he individual units.
+    }
+}
